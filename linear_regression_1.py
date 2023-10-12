274f5da77cf326311dc495693dcb0d24a3ec4257
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-12 18:35:48 +0530

    Third commit

--- linear_regression_1.py
+++ linear_regression_1.py
@@ -1,3 +1,22 @@
 import pandas as pd
+
 hb = pd.read_csv("headbrain.csv")
 print(hb)
+hb.info()
+hb.describe
+hb.shape
+hb.columns
+hb.dtypes
+hb.nunique()
+hb.isna().sum()
+# 1. ASSIGN
+X = hb[["Head Size(cm^3)"]].values
+y = hb[["Brain Weight(grams)"]].values
+# 1. ASSIGN
+X = hb[["Head Size(cm^3)"]].values
+y = hb[["Brain Weight(grams)"]].values
+# 2. SPLIT
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
